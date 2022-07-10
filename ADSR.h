d5a17c6e8fd9ddaa2e1fc4cfecbf6bb44c017f1b
Graham Richards <grahamdrichards@gmail.com>
2022-07-10 09:44:04 +0100

    trivial state machine

--- ADSR.h
+++ ADSR.h
@@ -1,35 +1,65 @@
 #pragma once
 
+enum State { idle, attack, decay, sustain, release };
+
 class ADSR {
     private:
     // config
     const int *samplerate;
-    const double *attackMs;
-    const double *decayMs;
+    const double *attackRate;
+    const double *decayRate;
     const double *sustainLevel;
-    const double *releaseMs;
+    const double *releaseRate;
 
     // state
     double output;
+    State state;
 
     public:
     ADSR(const int* samplerate, const double *attack, const double *decay, const double *sustain, const double* release) {
-        samplerate = samplerate;
-        attackMs = attack;
-        decayMs = decay;
-        sustainLevel = sustain;
-        releaseMs = release;
+        this->samplerate = samplerate;
+        this->attackRate = attack;
+        this->decayRate = decay;
+        this->sustainLevel = sustain;
+        this->releaseRate = release;
 
+        state = idle;
         output = 0.0;
     }
     double Tick(double input) {
+        switch(state) {
+            case idle: 
+                output = 0.0;
+            break;
+
+            case attack:
+                output += *attackRate / (double)*samplerate / 1000.0;
+                if (output > 1.0) {
+                    output = 1.0;
+                    state = sustain;
+                }
+            break;
+            case sustain:
+                output = 1.0;
+            break;
+            case release:
+                output -= *decayRate / (double)*samplerate / 1000.9;
+                if (output < 0) {
+                    output = 0.0;
+                    state = idle;
+                }
+            break;
+            default:
+                state = idle;
+                output = 0.0;
+        }
         return this->output;
     };
 
     void Trigger() {
-        this->output = 1.0;
+        this->state = attack;
     };
     void Release() {
-        this->output = 0.0;
+        this->state = release;
     };
 };
