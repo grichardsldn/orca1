aded84dcdb71aa1d4de4e022dc1dba5556d58867
Graham Richards <grahamdrichards@gmail.com>
2022-07-14 14:46:28 +0100

    massively reduced the damping

--- Filter.h
+++ Filter.h
@@ -20,7 +20,7 @@ class Filter {
     double Tick(double input) {
         input /= 2.;
         double diff = input - pos;
-        double restoring = (0 - pos) * 0.1;
+        double restoring = (0 - pos) * 0.00001;
         vel += (diff + restoring) / *samplerate * 1000.0 * *frequency;
         vel *= *resonance;
          
