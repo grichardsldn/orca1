768369cd54c526c4510d91f253c52fdfb4f4eacc
Graham Richards <grahamdrichards@gmail.com>
2022-08-27 17:42:28 +0100

    other lfo waveforms (#22)

--- LFO.h
+++ LFO.h
@@ -9,17 +9,19 @@ class LFO {
 
         through = 0.0;
     }
-    double Tick() {
-        const double periodSamples = *samplerate / *rate;
-        const double delta = 1.0/periodSamples;
-        through += delta;
-        if (delta > 1.0) {
-            // too fast
-            return 0.0;
-        }
-        if (through > 1.0) {
-        through -= 1.0;
-      }
+    private:
+
+    double currentOutput = 0.0;
+    double currentSampleAndHold = 0.0;
+    double lastSampleAndHold = 0.0;
+
+    double soften(double input) {
+        double delta = input - currentOutput;
+        currentOutput += delta / 10.0;
+        return currentOutput;
+    }
+
+    double triFunction(double through) {
         if (through > 0.75) {
             double subThrough = through - 0.75;
             return 1.0 - (subThrough * 4.0);
@@ -32,6 +34,50 @@ class LFO {
         }
         else {
             return (through * 4.0) *  -1.0;
+        }
+    }
+
+    double squareFunction(double through) {
+        if (through > 0.5) {
+            return -1.0;
+        } else {
+            return 1.0;
+        }
+    }
+
+
+    double randomRampFunction(double through) {
+        double diff = currentSampleAndHold - lastSampleAndHold;
+        return lastSampleAndHold + (through * diff);
+    }
+
+    public:
+
+    double Tick() {
+        const double periodSamples = *samplerate / *rate;
+        const double delta = 1.0/periodSamples;
+        through += delta;
+        if (delta > 1.0) {
+            // too fast
+            return 0.0;
+        }
+        if (through > 1.0) {
+            lastSampleAndHold = currentSampleAndHold;
+            currentSampleAndHold = ((double)(rand()%256)-128.0) / 128.0;
+            through -= 1.0;
+        }
+
+        switch (*waveform) {
+            case 0:
+                return soften(triFunction(through));
+            case 1:
+                return soften(squareFunction(through));
+            case 2:
+                return soften(currentSampleAndHold);
+            case 3:
+                return soften(randomRampFunction(through));
+            default:
+                return 0;
         }
     };
 
