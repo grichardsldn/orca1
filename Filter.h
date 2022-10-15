e0c12489fb8c72bd513fa28131bbd2f7322312ae
Graham Richards <grahamdrichards@gmail.com>
2022-10-15 17:41:27 +0100

    only input when within bounds

--- Filter.h
+++ Filter.h
@@ -23,6 +23,9 @@ class Filter {
         double frequency = 27.5 * pow(2, *octaveNumber);
         input /= 2.;
         double inputForce = input - pos;
+        if (pos > 1.0 || pos < -1.0) {
+            inputForce = 0;
+        }
         double restoringForce = (0 - pos) * 0.1; // low cut
 
         double mass = 2000.0 / (frequency * frequency);
@@ -35,14 +38,14 @@ class Filter {
         pos += impulse * (1.0 - *resonance) * 5.0;
         
         // end stops
-        if (pos > 1.0) {
-            pos = 1.0;
+        if (pos > 2.0) {
+            pos = 2.0;
             if (vel>0.0) {
                 vel = 0.0;
             }
         }
-        if (pos < -1.0) {
-            pos = -1.0;
+        if (pos < -2.0) {
+            pos = -2.0;
             if (vel<0.0) {
                 vel = 0.0;
             }
